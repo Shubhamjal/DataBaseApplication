a08268d12aa8c8dfecb5a15b56b1004de3eb6341
shubhamKumar <appscicseshubham22@gmail.com>
2024-08-08 09:22:20 +0530

    add data

--- app/src/main/java/com/example/databaseapplication/RecyclerActivity.kt
+++ app/src/main/java/com/example/databaseapplication/RecyclerActivity.kt
@@ -1,6 +1,10 @@
 package com.example.databaseapplication
 
+import android.app.Dialog
 import android.os.Bundle
+import android.util.EventLogTags.Description
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -37,6 +41,27 @@ class RecyclerActivity : AppCompatActivity() , RecyclerAdapter.onclick{
             this,LinearLayoutManager.VERTICAL,false)
         binding.recycler.adapter=recyclerAdapter
         getList()
+
+
+
+        binding.FabAdd.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.add_dialogbox)
+            val enter_rollno = dialog.findViewById<EditText>(R.id.EnterRollNo)
+            val enter_name = dialog.findViewById<EditText>(R.id.EnterName)
+            val add_btn = dialog.findViewById<Button>(R.id.Addbutton)
+            dialog.show()
+
+            add_btn.setOnClickListener {
+                val rollno = enter_rollno.text.toString()
+                val name_entered = enter_name.text.toString()
+                notesDatabase.notesInterface().insertTodo(NotesEntity(Title = rollno, Description = name_entered))
+                getList()
+                recyclerAdapter.notifyDataSetChanged()
+                dialog.dismiss()
+            }
+        }
+
     }
 
 
@@ -44,11 +69,29 @@ class RecyclerActivity : AppCompatActivity() , RecyclerAdapter.onclick{
 
 
     override fun update(position: Int) {
-        Toast.makeText(this, "Item Update", Toast.LENGTH_SHORT).show()
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.update_dialogbox)
+        val Rollno = dialog.findViewById<EditText>(R.id.UpdateRollNo)
+        val Name = dialog.findViewById<EditText>(R.id.UpdateName)
+        Rollno.setText(notesList[position].Title)
+        Name.setText(notesList[position].Description)
+        val update_btn = dialog.findViewById<Button>(R.id.Update_btn)
+        dialog.show()
 
+        update_btn.setOnClickListener {
+            val updated_rollno = Rollno.text.toString()
+            val updated_name = Name.text.toString()
 
+           notesDatabase.notesInterface().updateTodoEntity(NotesEntity(id = notesList[position].id, Title = updated_rollno, Description = updated_name))
 
+            getList()
+            recyclerAdapter.notifyDataSetChanged()
+            dialog.dismiss()
+        }
     }
+
+
+
 
     override fun delete(position: Int) {
         Toast.makeText(this, "Item Deleted", Toast.LENGTH_SHORT).show()
@@ -63,4 +106,6 @@ class RecyclerActivity : AppCompatActivity() , RecyclerAdapter.onclick{
         notesList.addAll(notesDatabase.notesInterface().getList())
 
     }
+
 }
+
